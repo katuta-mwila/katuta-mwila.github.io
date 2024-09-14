0e7392d31b4dec2438b597235c2403782cb7a736
Katuta Mwila <katutam.mwilam@gmail.com>
2024-09-15 10:30:46 +1200

    Fix images in javascript-dom blog

--- scripts/script.js
+++ scripts/script.js
@@ -10,10 +10,12 @@ function r255(){
 
 
 // create array containing names of different sports
-let sportsArray = ['Basketball', 'Netball', 'Rugby', 'Soccer']
+let sportsArray = ['Basketball', 'Netball', 'Rugby']
 
 // access the first word in the array using square brackets with the index of 0 inside
 console.log(sportsArray[0]) // this will output 'Basketball'
+console.log(sportsArray[1]) // this will output 'Netball'
+console.log(sportsArray[2]) // this will output 'Rugby'
 
 
 // create a person object with three pieces of data
@@ -25,6 +27,6 @@ let personObject = {
 }
 
 // access each piece of data by seperating the name of the object and the keys by a period
-console.log(personObject.name) // outputs 'john'
+console.log(personObject.name) // outputs 'John'
 console.log(personObject.age) // outputs '30'
 console.log(personObject.job) // outputs 'Plumber'
